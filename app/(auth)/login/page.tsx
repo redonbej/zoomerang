9ffdf98c82cd5a62cb27962ffaca629b495913f3
redon <redonbejtullahu@gmail.com>
2024-11-20 21:47:47 +0100

    add auth pages (login,register), room pages

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,18 @@
+const Login = () => {
+    return <div className="flex justify-between p-10">
+        <div>Logo</div>
+
+        <div>
+            <button className="px-2 py-1 rounded bg-blue-200 border border-blue-50 mr-2.5">button1</button>
+            <button className="px-2 py-1 rounded bg-blue-200 border border-blue-50 mr-2.5">vutton2</button>
+            <button className="px-2 py-1 rounded bg-blue-200 border border-blue-50 mr-2.5">vutton2</button>
+        </div>
+
+        <div>
+            assssadasdassaddasasdsad
+        </div>
+
+    </div>
+}
+
+export default Login;
