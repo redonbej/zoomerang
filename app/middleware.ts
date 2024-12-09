a37e8948376dbb58a06bbe9520e28d28d20734d5
erakastrati <erazkastrati@gmail.com>
2024-12-09 13:39:25 +0100

    attach user data to request for valid tokens

--- app/middleware.ts
+++ app/middleware.ts
@@ -1,27 +1,33 @@
 import { NextRequest, NextResponse } from 'next/server';
 import jwt from 'jsonwebtoken';
+import prisma from '@/lib/prisma';
 
+export async function middleware(request: NextRequest) {
+  const token = request.headers.get('token');
 
-export function middleware(request: NextRequest) {
-    const token = request.headers.get('token');
-  
-    if (!token) {
-      console.log('Token is missing');
-      return NextResponse.redirect(new URL('/login', request.url));
-    }
-  
-    try {
-      const decoded: any = jwt.decode(token);
-  
-      if (!decoded || decoded.exp * 1000 < Date.now()) {
-        console.log('Token expired');
-        return NextResponse.redirect(new URL('/login', request.url));
-      }
-  
-      console.log('Token is valid');
-      return NextResponse.next();
-    } catch (error) {
-      console.error('Error decoding token:', error);
-      return NextResponse.redirect(new URL('/login', request.url));
-    }
+  if (!token) {
+    console.log('Token is missing');
+    return NextResponse.redirect(new URL('/login', request.url));
   }
+
+  try {
+    const decoded: any = jwt.decode(token);
+
+    if (!decoded || decoded.exp * 1000 < Date.now()) {
+      console.log('Token expired');
+      return NextResponse.redirect(new URL('/login', request.url));
+    }
+
+    console.log('Token is valid');
+    const user = await prisma.user.findMany();
+    request.headers.set('user', JSON.stringify(user));
+    return NextResponse.next();
+  } catch (error) {
+    console.error('Error handling token:', error);
+    return NextResponse.redirect(new URL('/login', request.url));
+  }
+}
+
+export const config = {
+  matcher: '*',
+};
