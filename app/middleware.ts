b3251e2cc94f7014efbd4aeb1683ecd3d17e167d
erakastrati <erazkastrati@gmail.com>
2024-12-09 10:14:12 +0100

    extract token from request headers in middleware

--- app/middleware.ts
+++ app/middleware.ts
@@ -1,9 +1,13 @@
 import { NextRequest, NextResponse } from 'next/server';
 
 export function middleware(request: NextRequest) {
-  return NextResponse.next();
-}
-
-export const config = {
-  matcher: '*',
-};
+    const token = request.headers.get('token');
+  
+    if (!token) {
+      console.log('Token is missing');
+      return NextResponse.redirect(new URL('/login', request.url));
+    }
+  
+    console.log('Token:', token);
+    return NextResponse.next();
+  }
