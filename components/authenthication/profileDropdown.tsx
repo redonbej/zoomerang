857eaecfd27656b95ef0795a7692a82810527df6
tarantula-valon <valonidv10@hotmail.com>
2024-12-18 20:42:00 +0100

    Added dropdown for user

---
+++ components/authenthication/profileDropdown.tsx
@@ -0,0 +1,32 @@
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import { LogOutIcon, UserIcon } from "lucide-react";
+import Link from "next/link";
+import { useAuth } from "@/app/context/AuthContext";
+import { useRouter } from "next/navigation";
+
+export default function ProfileDropdown() {
+    const { handleLogOut } = useAuth();  
+    const router = useRouter();
+
+    // Handle log out
+    const handleLogOutClick = () => {
+        handleLogOut(); 
+        router.push("/login"); 
+    };
+
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger>My name</DropdownMenuTrigger>
+            <DropdownMenuContent>
+                <DropdownMenuItem>
+                    <Link href="/profile" className="text-base flex items-center gap-2">
+                        <UserIcon className="size-5" /> My profile
+                    </Link>
+                </DropdownMenuItem>
+                <DropdownMenuItem onClick={handleLogOutClick} className="cursor-pointer text-base flex items-center gap-2">
+                    <LogOutIcon className="!size-5" /> Sign Out
+                </DropdownMenuItem>
+            </DropdownMenuContent>
+        </DropdownMenu>
+    );
+}
