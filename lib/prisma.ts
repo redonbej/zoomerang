3e839f2ec84efa62565a0200022df460d30a6335
erakastrati <erazkastrati@gmail.com>
2025-01-05 14:50:59 +0100

    Implemented Singleton pattern for Prisma client

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,14 +1,40 @@
+// changed for the reccommeded wat for singleton pattern, need to check if it works
+
+// import { PrismaClient } from '@prisma/client';
+
+// let prisma: PrismaClient;
+
+// if (process.env.NODE_ENV === 'production') {
+//     prisma = new PrismaClient();
+// } else {
+//     if (!global.prisma) {
+//         global.prisma = new PrismaClient();
+//     }
+//     prisma = global.prisma;
+// }
+
+// export default prisma;
+
 import { PrismaClient } from '@prisma/client';
 
-let prisma: PrismaClient;
+class PrismaSingleton {
+  private static instance: PrismaClient;
 
-if (process.env.NODE_ENV === 'production') {
-    prisma = new PrismaClient();
-} else {
-    if (!global.prisma) {
-        global.prisma = new PrismaClient();
+  private constructor() {}
+
+  public static getInstance(): PrismaClient {
+    if (!PrismaSingleton.instance) {
+      if (process.env.NODE_ENV === 'production') {
+        PrismaSingleton.instance = new PrismaClient();
+      } else {
+        if (!(global as any).prisma) {
+          (global as any).prisma = new PrismaClient();
+        }
+        PrismaSingleton.instance = (global as any).prisma;
+      }
     }
-    prisma = global.prisma;
+    return PrismaSingleton.instance;
+  }
 }
 
-export default prisma;
+export default PrismaSingleton.getInstance();
