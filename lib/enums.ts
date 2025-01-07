6f22d955185951933444a92d096b568ef2c4c7fd
erakastrati <erazkastrati@gmail.com>
2025-01-07 20:20:46 +0100

    Enums for Socket Event Types

---
+++ lib/enums.ts
@@ -0,0 +1,15 @@
+export enum WebRTCMessageType {
+    Join = 'join',
+    Quit = 'quit',
+    SendOffer = 'send_offer',
+    SendAnswer = 'send_answer',
+    SendIceCandidate = 'send_ice_candidate',
+    MessageSend = 'message_send',
+    Joined = 'joined',
+    OfferSdpReceived = 'offer_sdp_received',
+    AnswerSdp = 'answer_sdp',
+    IceCandidateReceived = 'ice_candidate_received',
+    MessageReceived = 'message_received',
+    Left = 'left'
+  }
+  
