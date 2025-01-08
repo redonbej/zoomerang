fcab68bfec86e8a41565fd13c52f6792cf2c7e2b
erakastrati <erazkastrati@gmail.com>
2025-01-08 21:39:13 +0100

    implement decorator design pattern in axios interceptors

--- lib/interceptor.ts
+++ lib/interceptor.ts
@@ -4,27 +4,33 @@ const axiosInstance = axios.create({
     baseURL: "/api", 
 });
 
-axiosInstance.interceptors.request.use(
-    (config) => {
-        const token = localStorage.getItem("token");
-        if (token) {
-            config.headers.Authorization = `Bearer ${token}`;
-        }
-        return config;
-    },
-    (error) => {
-        return Promise.reject(error);
-    }
-);
 
-axiosInstance.interceptors.response.use(
-    (response) => response,
-    (error) => {
-        if (error.response?.status === 401) {
-            console.error("Unauthorized - logging out user...");
-        }
-        return Promise.reject(error);
+const requestDecorator = (next) => (config) => {
+    console.log("Request Decorator: Adding Authorization Token");
+    const token = localStorage.getItem("token");
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
     }
+    return next(config);
+};
+
+const responseDecorator = (next) => (response) => {
+    console.log("Response Decorator: Intercepting response");
+    return next(response);
+};
+
+const errorDecorator = (next) => (error) => {
+    if (error.response?.status === 401) {
+        console.error("Unauthorized - logging out user...");
+    }
+    return next(error);
+};
+
+
+axiosInstance.interceptors.request.use(requestDecorator((config) => config));
+axiosInstance.interceptors.response.use(
+    responseDecorator((response) => response),
+    errorDecorator((error) => Promise.reject(error))
 );
 
 export default axiosInstance;
